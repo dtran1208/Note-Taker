f6d1c2d26ca98f406ab22ca6bfba898165687eb8
dtran1208 <ducminh.tran08@gmail.com>
2023-05-09 00:30:27 -0400

    add file

---
+++ public/assets/js/server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile("db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile("db.json", JSON.stringify(updatedNotes), (err) => {
+      if (err) throw err;
+      res.json({ message: "Note deleted" });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
